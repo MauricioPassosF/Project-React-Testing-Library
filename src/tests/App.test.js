976fe784f7425c7ed398b515b52246bf3d29b829
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-03 16:09:37 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,48 @@
-test('', () => {});
-// Testes
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  it('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavPoke).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About, na URL /about ao clicar no link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémon Favoritados, na URL /favorites, ao clicar no link Favorite Pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(linkFavPoke);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => { history.push('/invalid'); });
+    const notFoundText = await screen.findByRole('heading', {
+      name: 'Page requested not found',
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
