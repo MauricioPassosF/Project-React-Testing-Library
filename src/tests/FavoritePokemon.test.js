ce9ed44ae04473fbc086d0c218a2d4486099f912
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-04 17:41:21 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { FavoritePokemon } from '../pages';
+import pokemonList from '../data';
 
 describe('Componente <FavoritePokemon.js />', () => {
   it('Exibe-se na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', async () => {
-    renderWithRouter(<App />);
-    const linkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémon' });
-    userEvent.click(linkFavPoke);
-    const noFavText = await screen.findByText('No favorite Pokémon found');
-    expect(noFavText).toBeInTheDocument();
+    renderWithRouter(<FavoritePokemon />);
+    screen.getByText(/No favorite Pokémon found/i);
   });
 
-  // it.only('Apenas são exibidos os Pokémon favoritados', () => {
-  //   const { history } = renderWithRouter(<App />);
-  //   const linkDetails = screen.getByRole('link', { name: 'More details' });
-  //   userEvent.click(linkDetails);
-  //   const checkboxFavoritePoke = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
-  //   userEvent.click(checkboxFavoritePoke);
-  //   act(() => { history.push('/favorites'); });
-
-  // });
+  it('Apenas são exibidos os Pokémon favoritados', () => {
+    renderWithRouter(<FavoritePokemon
+      pokemonList={ pokemonList.filter((poke, index) => index < 2) }
+    />);
+    const pokesName = screen.getAllByTestId('pokemon-name');
+    const pokesType = screen.getAllByTestId('pokemon-type');
+    expect(pokesName).toHaveLength(2);
+    expect(pokesType).toHaveLength(2);
+    screen.getByText(/pikachu/i);
+    screen.getByText(/charmander/i);
+  });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Componente <Pokedex.js />', () => {
+  it('A página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    const headingPokedex = screen.getByRole('heading', {
+      name: 'Encountered Pokémon',
+      level: 2,
+    });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+
+  it('Exibe-se o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
+    const { history } = renderWithRouter(<App />);
+    const buttonNextPoke = screen.getByTestId('next-pokemon');
+    // console.log(buttonNextPoke);
+    expect(buttonNextPoke).toBeInTheDocument();
+    console.log(history);
+    userEvent.click(buttonNextPoke);
+    console.log(history);
+    // expect(buttonNextPoke).toHaveProperty('name', 'Próximo Pokémon');
+  });
+
+  it.skip('Mostra-se apenas um Pokémon por vez', () => {
+  });
+
+  it.skip('A Pokédex tem os botões de filtro', () => {
+  });
+
+  it.skip('A Pokédex contém um botão para resetar o filtro', () => {
+  });
+});
