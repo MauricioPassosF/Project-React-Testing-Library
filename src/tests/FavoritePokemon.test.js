78d4ef88aa2834f08102f3e2d009a51593874596
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-03 21:59:14 -0300

    Requisito 4

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Componente <FavoritePokemon.js />', () => {
+  it('Exibe-se na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', async () => {
+    renderWithRouter(<App />);
+    const linkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(linkFavPoke);
+    const noFavText = await screen.findByText('No favorite Pokémon found');
+    expect(noFavText).toBeInTheDocument();
+  });
+
+  // it.only('Apenas são exibidos os Pokémon favoritados', () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const linkDetails = screen.getByRole('link', { name: 'More details' });
+  //   userEvent.click(linkDetails);
+  //   const checkboxFavoritePoke = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+  //   userEvent.click(checkboxFavoritePoke);
+  //   act(() => { history.push('/favorites'); });
+
+  // });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const headingNotFound = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+
+  it('a página mostra a imagem correta', () => {
+    render(<NotFound />);
+    const imageNotFound = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(imageNotFound).toBeInTheDocument();
+    expect(imageNotFound).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
