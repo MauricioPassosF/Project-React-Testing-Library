f84c0784bdd3d658c9353d5e70e7fe7f3cc4a2b8
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-04 22:23:46 -0300

    Requisitos 5 e 6 não finalizados

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,37 +1,108 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { Pokedex } from '../pages';
+import pokemonList from '../data';
 
 describe('Componente <Pokedex.js />', () => {
+  const testIdName = 'pokemon-name';
+  const testIdNext = 'next-pokemon';
   it('A página contém um heading h2 com o texto Encountered Pokémon', () => {
-    renderWithRouter(<App />);
-    const headingPokedex = screen.getByRole('heading', {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2) }
+      isPokemonFavoriteById={ { 25: true, 4: true } }
+    />);
+    screen.getByRole('heading', {
       name: 'Encountered Pokémon',
       level: 2,
     });
-    expect(headingPokedex).toBeInTheDocument();
   });
 
-  it('Exibe-se o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
-    const { history } = renderWithRouter(<App />);
-    const buttonNextPoke = screen.getByTestId('next-pokemon');
-    // console.log(buttonNextPoke);
-    expect(buttonNextPoke).toBeInTheDocument();
-    console.log(history);
+  it('Exibe-se o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2) }
+      isPokemonFavoriteById={ { 25: true, 4: true } }
+    />);
+    const buttonNextPoke = screen.getByTestId(testIdNext);
+    expect(buttonNextPoke).toHaveTextContent('Próximo Pokémon');
+    const firstPoke = screen.getByTestId(testIdName);
+    expect(firstPoke).toHaveTextContent('Pikachu');
     userEvent.click(buttonNextPoke);
-    console.log(history);
-    // expect(buttonNextPoke).toHaveProperty('name', 'Próximo Pokémon');
+    const secondPoke = await screen.findByTestId(testIdName);
+    expect(secondPoke).toHaveTextContent('Charmander');
+    userEvent.click(buttonNextPoke);
+    const thirdPoke = await screen.findByTestId(testIdName);
+    expect(thirdPoke).toHaveTextContent('Pikachu');
   });
 
-  it.skip('Mostra-se apenas um Pokémon por vez', () => {
+  it('Mostra-se apenas um Pokémon por vez', async () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2) }
+      isPokemonFavoriteById={ { 25: true, 4: true } }
+    />);
+    const buttonNextPoke = screen.getByTestId(testIdNext);
+    const pokesFirst = screen.getAllByTestId(testIdName);
+    expect(pokesFirst).toHaveLength(1);
+    userEvent.click(buttonNextPoke);
+    const pokesSecond = await screen.findAllByTestId(testIdName);
+    expect(pokesSecond).toHaveLength(1);
   });
 
-  it.skip('A Pokédex tem os botões de filtro', () => {
+  it('A Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2 || index === 6) }
+      isPokemonFavoriteById={ { 25: true, 4: true, 6: true } }
+    />);
+    const buttonsFilter = screen.getAllByTestId('pokemon-type-button');
+    expect(buttonsFilter).toHaveLength(2);
+    expect(buttonsFilter[0]).toHaveTextContent(/electric/i);
+    expect(buttonsFilter[1]).toHaveTextContent(/fire/i);
   });
 
-  it.skip('A Pokédex contém um botão para resetar o filtro', () => {
+  // it.only('A Pokédex não tem os botões de filtro sem os pokémons', () => {
+  //   renderWithRouter(<Pokedex
+  //     pokemonList={ [pokemonList
+  //       .filter((poke, index) => index < 2 || index === 6)
+  //       .map(({ id }) => id)] }
+  //     isPokemonFavoriteById={ { 25: true, 4: true, 6: true } }
+  //   />);
+  //   const buttonsFilter = screen.getAllByTestId('pokemon-type-button');
+  //   expect(buttonsFilter).not.toBeDefined();
+  // });
+
+  it('A Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2 || index === 6) }
+      isPokemonFavoriteById={ { 25: true, 4: true, 6: true } }
+    />);
+    screen.getByRole('button', { name: /all/i });
+  });
+
+  it('A Pokédex tem botões que funcionam corretamente', async () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList.filter((poke, index) => index < 2 || index === 6) }
+      isPokemonFavoriteById={ { 25: true, 4: true, 6: true } }
+    />);
+    const firstUnfilteredPoke = screen.getByTestId(testIdName);
+    expect(firstUnfilteredPoke).toHaveTextContent('Pikachu');
+    const buttonNextPoke = screen.getByTestId(testIdNext);
+    userEvent.click(buttonNextPoke);
+    const secondUnfilteredPoke = await screen.findByTestId(testIdName);
+    expect(secondUnfilteredPoke).toHaveTextContent('Charmander');
+    const buttonsFilter = screen.getAllByTestId('pokemon-type-button');
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    userEvent.click(buttonsFilter[1]);
+    const firstFilteredPoke = await screen.findByTestId(testIdName);
+    expect(firstFilteredPoke).toHaveTextContent('Charmander');
+    userEvent.click(buttonNextPoke);
+    const secondFilteredPoke = await screen.findByTestId(testIdName);
+    expect(secondFilteredPoke).toHaveTextContent('Rapidash');
+    userEvent.click(buttonAll);
+    const firstAllPoke = await screen.findByTestId(testIdName);
+    expect(firstAllPoke).toHaveTextContent('Pikachu');
+    userEvent.click(buttonNextPoke);
+    const secondAllPoke = await screen.findByTestId(testIdName);
+    expect(secondAllPoke).toHaveTextContent('Charmander');
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { Pokemon } from '../components';
+import pokemonList from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Componente <Pokemon.js />', () => {
+  it('Renderiza-se um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemonList[0] } isFavorite={ false } />);
+    const name = screen.getByTestId('pokemon-name');
+    expect(name).toHaveTextContent('Pikachu');
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toHaveTextContent('Electric');
+    const weight = screen.getByTestId('pokemon-weight');
+    expect(weight).toHaveTextContent('Average weight: 6.0 kg');
+    const imageAbout = screen.getByRole('img', { name: 'Pikachu sprite' });
+    expect(imageAbout).toBeInTheDocument();
+    expect(imageAbout).toHaveProperty('src', 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('O card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes', async () => {
+    const { history } = renderWithRouter(<Pokemon
+      pokemon={ pokemonList[0] }
+      isFavorite={ false }
+    />);
+    const linkDetails = screen.getByRole('link', { name: /more details/i });
+    expect(linkDetails).toBeInTheDocument();
+    userEvent.click(linkDetails);
+    expect(history.location.pathname).toBe('/pokemon/25');
+  });
+
+  it('Existe um ícone de estrela nos Pokémon favoritados', () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemonList[0] }
+      isFavorite
+    />);
+    const imageFav = screen.getByRole('img', { name: 'Pikachu is marked as favorite' });
+    expect(imageFav).toBeInTheDocument();
+    expect(imageFav).toHaveProperty('src', 'http://localhost/star-icon.svg');
+  });
+
+  it('Pokémon não favoritados não tem ícone de estrela', () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemonList[0] }
+      isFavorite={ false }
+    />);
+    expect(screen.getByRole('img', { name: 'Pikachu is marked as favorite' })).toBeUndefined();
+  });
+});
